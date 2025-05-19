4a10cd278f5cc2bc97f863e09b6c0eed5004e5e1
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2025-05-19 21:22:29 +0530

    IFE non dynamic updated.

---
+++ IFE/NonDynamic/richLoads/2025_JetBlueRetail_IFE_160x600__NonDynamic_RL/mainRL1.js
@@ -0,0 +1,73 @@
+var tl;
+var tl2;
+var container = myFT.$("#main_container");
+var default_exit = myFT.$("#default_exit");
+
+// var myVid=myFT.$("#video1");
+var clickTag1_url="";
+// var vid_time_triggered=false;
+
+var startTime;
+
+//
+default_exit.on('click',function(){
+  myFT.clickTag(1,clickTag1_url)
+})
+// wait for instantads to load before initializing creative animation
+myFT.on('instantads',function(){
+
+  clickTag1_url=myFT.instantAds.clickTag1_url;
+
+})
+
+init()
+function animate() {
+  tl.set(["#main_content"], { autoAlpha: 1, force3D: true });
+
+  tl.set(['#h1, #h2'], {y:"20px"})
+  tl.set(['#seat1, #seat2'], {y:"100%"})
+  
+  tl.addLabel('frame1', 0)
+  .to('#h1', 0.5, { autoAlpha: 1, y: "0", ease: Power1.easeOut}, 'frame1+=0.5')
+  .to('#seat1', 0.5, { autoAlpha: 1, y: "0", ease: Power1.easeOut}, '<')
+
+  .addLabel('frame2', 'frame1+=4')
+  .to('#h1', 0.5, { autoAlpha: 0, y: "20px", ease: Power1.easeOut }, "frame2")
+  .to('#seat1', 0.5, { autoAlpha: 0, y: "20%", ease: Power1.easeOut}, '<')
+  .to(['#h2, #term2'], 0.5, { autoAlpha: 1, y: "0", ease: Power1.easeOut }, "frame2+=0.5")
+  .to('#seat2', 0.5, { autoAlpha: 1, y: "0", ease: Power1.easeOut}, '<')
+
+}
+
+function setRollover() {
+  document.getElementById('default_exit').addEventListener('mouseover', defaultOver, false);
+  document.getElementById('default_exit').addEventListener('mouseout', defaultOut, false);
+}
+
+function defaultOver() {
+  TweenMax.to('#cta', 0.15, { scale: 1.1, ease: Power1.easeInOut })
+}
+
+function defaultOut() {
+  TweenMax.to('#cta', 0.15, { scale: 1, ease: Power1.easeInOut })
+}
+
+function init() {
+  startTime = new Date();
+  tl = new TimelineMax({onComplete: logDuration});
+
+  animate();
+  setRollover();
+		
+}
+
+function logDuration() {
+  let endTime = new Date();
+  console.log(
+    "Animation duration: " + ((endTime - startTime) / 1000).toFixed(2) + " seconds"
+  );
+}
+
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("year").innerHTML = year;
