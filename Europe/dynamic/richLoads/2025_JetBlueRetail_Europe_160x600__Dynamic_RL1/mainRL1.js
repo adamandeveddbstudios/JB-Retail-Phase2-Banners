b1ef4466aca25b8679cbd9fd78e8ed67eccf17cd
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2025-05-27 21:49:11 +0530

    animation

--- Europe/dynamic/richLoads/2025_JetBlueRetail_Europe_160x600__Dynamic_RL1/mainRL1.js
+++ Europe/dynamic/richLoads/2025_JetBlueRetail_Europe_160x600__Dynamic_RL1/mainRL1.js
@@ -99,15 +99,12 @@ function animate() {
   .to('#h1', 0.5, { autoAlpha: 0, y: "20px", ease: Power1.easeOut }, "frame2")
   .to('#h2', 0.5, { autoAlpha: 1, y: "0", ease: Power1.easeOut }, "frame2+=0.5")
 
-  // .addLabel('frame_END', "frame2+=5")
-  // .to('#endframeBg', 0.6 ,{ top: 0, ease: Back.easeOut.config(.3)}, 'frame_END')
+  .addLabel('frame_END', "frame2+=5")
+  .to('#endframeBg', 0.6 ,{ top: 0, ease: Back.easeOut.config(.3)}, 'frame_END')
 
-  // const totalDuration = tl.duration();
-  // tl.fromTo("#cloud", totalDuration, { x: "30" }, { x: "0", ease: Power0.easeNone }, 0);
-  // tl.fromTo("#plane", totalDuration, { x: "-100" }, { x: "0", ease: Power0.easeNone }, 0)
   //////////////////////////////////////
   // @FT2 code block start
-  // .call(playEndframe, ["param1"], 'frame_END')
+  .call(playEndframe, ["param1"], 'frame_END')
   // @FT2 code block end
   ////////////////////////////////////////
 
